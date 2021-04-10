0bae236da952785386bf8bfc58649ecb08a27bee
ac1dc0de <43935332+ac1dc0de@users.noreply.github.com>
2021-04-10 13:23:56 +0200

    Update aosp_build_pre.sh

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -13,9 +13,19 @@ rm -rf "${community_patches_dir}"
 git clone https://github.com/rattlesnakeos/community_patches "${community_patches_dir}"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00002-round-icon.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00003-enable-volte-wifi-calling.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00004-use-cloudflare-dns.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00005-2-button-navigation.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00006-increase-default-maximum-password-length.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00007-set-seedvault-as-dftl-bkp-provider.patch"
 
 ## apply custom hosts file
-#custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-#echo "applying custom hosts file ${custom_hosts_file}"
-#retry wget -q -O "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts" "${custom_hosts_file}"
+custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+echo "applying custom hosts file ${custom_hosts_file}"
+retry wget -q -O "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts" "${custom_hosts_file}"
+
+# Add custom CA
+CAroot="https://raw.githubusercontent.com/ac1dc0de/custom-config-repo/main/5ed36f99.0"
+wget $CAroot -P "${AOSP_BUILD_DIR}/system/ca-certificates/files/"
+CAclass3="https://raw.githubusercontent.com/ac1dc0de/custom-config-repo/main/e5662767.0"
+wget $CAclass3 -P "${AOSP_BUILD_DIR}/system/ca-certificates/files/"
