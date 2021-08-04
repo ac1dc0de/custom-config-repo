b018a94f8fbeba927fe747d63bb968c9e11d1db8
ac1dc0de <43935332+ac1dc0de@users.noreply.github.com>
2021-08-04 21:28:31 +0200

    Update build_chromium_pre.sh

--- hooks/build_chromium_pre.sh
+++ hooks/build_chromium_pre.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
 # apply patches prior to chromium build. note: this increases likelyhood of build failures.
 #echo "applying vanadium patches"
+echo "exclude applying vanadium patches"
 #vanadium_dir="${ROOT_DIR}/vanadium"
 #git clone https://github.com/GrapheneOS/Vanadium.git "${vanadium_dir}"
 #cd "${CHROMIUM_BUILD_DIR}/src"
